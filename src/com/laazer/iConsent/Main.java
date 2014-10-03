ca99e34efc7d0714480d0b76b1494568b33946a3
Jacob <jacob@Jacobs-MacBook-Pro.local>
2014-10-02 20:52:39 -0400

    first commit

---
+++ src/com/laazer/iConsent/Main.java
@@ -0,0 +1,21 @@
+package com.laazer.iConsent;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, VidCap.class));
+    }
+}
---
+++ src/com/laazer/iConsent/Start.java
@@ -0,0 +1,15 @@
+package com.laazer.iConsent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jacob on 10/1/14.
+ */
+public class Start extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);
+    }
+}
